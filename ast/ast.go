391fd650fe06c2f7fa5b5dba1c06066b6c7d9bff
sayandip-dutta <sayandip.dutta@nevaehtech.com>
2024-08-13 14:09:14 +0530

    add Program, LetStatement, and Identifier Node

--- ast/ast.go
+++ ast/ast.go
@@ -1,15 +1,52 @@
 package ast
 
+import "github.com/sayandipdutta/monkey/token"
+
 type Node interface {
 	TokenLiteral() string
 }
 
 type Statement interface {
 	Node
-	StatementNode()
+	statementNode()
 }
 
 type Expression interface {
 	Node
-	ExpressionNode()
+	expressionNode()
+}
+
+// Program
+type Program struct {
+	Statements []Statement
+}
+
+func (prog *Program) TokenLiteral() string {
+	if len(prog.Statements) > 0 {
+		return prog.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+// LetStatment
+type LetStatment struct {
+	Name  *Identifier
+	Value Expression
+	Token token.Token
+}
+
+func (stmt *LetStatment) statementNode() {}
+func (stmt *LetStatment) TokenLiteral() string {
+	return stmt.Token.Literal
+}
+
+// Identifier
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (ident *Identifier) expressionNode() {}
+func (ident *Identifier) TokenLiteral() string {
+	return ident.Token.Literal
 }
